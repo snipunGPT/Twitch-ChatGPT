283caf20dcc0d11b537874d4beefd8c6178f06c0
snipunGPT <135639042+snipunGPT@users.noreply.github.com>
2023-06-05 14:17:53 -0700

    Add files via upload

--- chatGPT.py
+++ chatGPT.py
@@ -11,11 +11,12 @@ openai.api_key = credentials.MY_API_KEY
   
 
 class NugGPT:
-    def __init__(self, app_id, app_secret, user_scope, target_channel):
+    def __init__(self, app_id, app_secret, user_scope, target_channel, bot_name):
         self.app_id = app_id
         self.app_secret = app_secret
         self.user_scope = user_scope
         self.target_channel = target_channel
+        self.bot = BOT_NAME
         self.twitch = None
         self.chat = None
 
@@ -28,14 +29,18 @@ class NugGPT:
     async def on_message(self, msg: ChatMessage):
         print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
         message = msg.text.split()
-        if msg.text[0] == '!' and message[0] not in commandList.commands:
-            print('found a command to make up!', message[0])
-            prompt = message[0]
+        if self.bot in message:
+            print('responding to a convo with, ', {msg.user.name})
+            prompt = msg.text
             with open('personality.txt', 'r+') as personality:
                 identity = personality.read()
-            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"Pretend you have the personality of a {identity} and reply to {prompt}."}])
-            # Replace the apostrophe'd item below with whatever Twitch channel name (NOT the URL) you want the bot to respond to
-            await self.chat.send_message('replace_with_the_twitch_channel_name', completion.choices[0].message.content)
+            while True:
+                try:
+                    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"Pretend you have the personality of a {identity} and reply to {prompt} as if it were a command."}], temperature=.8, top_p=0.5)
+                    await self.chat.send_message(self.target_channel, completion.choices[0].message.content)
+                    break
+                except Exception as e:
+                    print("An error occurred: ", e)
 
     async def run(self):
         self.twitch = await Twitch(self.app_id, self.app_secret)
@@ -58,9 +63,9 @@ if __name__ == '__main__':
     APP_SECRET = config.APP_SECRET
     USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT, AuthScope.MODERATOR_MANAGE_BANNED_USERS]
     # Replace the apostrophe'd item below with whatever Twitch channel name (NOT the URL) you want the bot to respond to
-    TARGET_CHANNEL = 'replace_with_the_twitch_channel_name'
+    TARGET_CHANNEL = 'REPLACE'
     # Replace the apostrophe'd item below with your bot's username making sure to include the @ sign
-    BOT_NAME = '@replace_with_bot_name'
+    BOT_NAME = '@REPLACE'
 
     nugexe = NugGPT(APP_ID, APP_SECRET, USER_SCOPE, TARGET_CHANNEL, BOT_NAME)
     asyncio.run(nugexe.run())
